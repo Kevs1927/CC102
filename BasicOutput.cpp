e49201d3437b0889db2b9a4eda6c97321cf4b5f2
Kevs1927 <162280524+Kevs1927@users.noreply.github.com>
2024-10-03 09:18:23 +0800

    Add files via upload

---
+++ BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "BS Information Technology, 1st Year" << endl;
+	cout << "John Kevin A. Mabeza" << endl;
+	cout << "P-6, Brgy. Borabod Daet Camarines Norte" << endl;
+	return 0;
+}
