d5fac97972c60a8f49136460c017a1e91012e8a8
John Kevin A. Mabeza <162280524+Kevs1927@users.noreply.github.com>
2024-11-13 08:08:05 +0800

    Add files via upload

---
+++ TemperatureElIf.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int response;
+	cout << "What is the Temperature outside?: ";
+	cin >> response;
+	if (response <= 32) {
+		cout << "Bring a heavy jacket";
+	}
+	else if (response == 32 || response <= 50) {
+		cout << "Bring light jacket";
+	}
+	else if (response > 50) {
+		cout << "No need to bring jacket";
+	} 
+	
+}
