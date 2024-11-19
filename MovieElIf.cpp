3b2d53d3c92394d1dabb3e2cf8bff8258ba7e180
John Kevin A. Mabeza <162280524+Kevs1927@users.noreply.github.com>
2024-11-19 11:24:13 +0800

    Update MovieElIf.cpp

--- MovieElIf.cpp
+++ MovieElIf.cpp
@@ -4,23 +4,28 @@ using namespace std;
 
 int main()
 {
-	int age;
-	bool parents;
-	double money;
-	char time;
-	cout << "Enter age: ";
-	cin >> age;
-	cout << "Enter money: ";
-	cin >> money;
-	if (age < 13) {
-		cout << "With parents?: ";
-		cin >> parents;
-		cout << ((parents) ? "Rated g" : "Rated g, pg")<<endl;
-	}
-	else if (age>=13 && age<16) {
-		cout << "With parents?: ";
-		cin >> parents;
-		cout << ((parents) ? "Rated g,pg" : "Rated g, pg or r") <<endl;
-	}
-	cout<< ((money<7.50)? "Not enough money!": ((money<10.50)? "Can go to matinee show!": "Can go to metinee or evening!"))<<endl;
+    int age;
+    bool parents;
+    double money;
+    char withParents;
+    cout << "Enter age: ";
+    cin >> age;
+    cout << "Enter money: ";
+    cin >> money;
+    if (age < 13) {
+        cout << "With parents? (y/n): ";
+        cin >> withParents;
+        cout << (parents ? "Rated G and PG" : "Rated G") << endl;
+    }
+    else if (age >= 13 && age < 16) {
+        cout << "With parents? (y/n): ";
+        cin >> withParents;
+        parents = (withParents == 'y' || withParents == 'Y');
+        cout << (parents ? "Rated G, PG and R" : "Rated G and PG") << endl;
+    }
+    else {
+        cout << "Rated G, PG, and R" << endl;
+    }
+    cout << (money < 7.50 ? "Not enough money!" : (money < 10.50 ? "Can go to matinee show!" : "Can go to matinee or evening!")) << endl;
+    return 0;
 }
